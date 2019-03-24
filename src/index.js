f4ee57a0f1abda275bef516e4c5705d52f9f6f0a
Nattalia Mozhaeva <you@example.com>
2019-03-24 07:58:44 +0300

    feat: add decision fail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange(money) {
+   if (money > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+   if (money === 0) return {};
+ const result = {}
+    if (money >= 50) {
+      result["H"] = Math.floor(money / 50);
+      money = money - Math.floor(money / 50)*50;
+    }
+    if (money >= 25) {
+      result["Q"]= Math.floor(money/ 25);
+      money = money - Math.floor(money/25)*25;
+    }
+    if (money >= 10) {
+      result["D"] = Math.floor(money / 10);
+      money = money - Math.floor(money / 10)*10;
+    }
+    if (money >= 5) {
+      result["N"] = Math.floor (money / 5);
+      money = money - Math.floor (money / 5)*5;
+    }
+    if (money >= 1){
+      result["P"] = money;
+    }
+    return result;
 }
